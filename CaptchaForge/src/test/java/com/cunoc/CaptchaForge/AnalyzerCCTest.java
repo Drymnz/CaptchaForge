e9abe2d413ae789dfccb2227b702ab9ac2893363
Benjamín de Jesús <benjaminperez201831504@cunoc.edu.gt>
2024-10-05 02:43:02 -0600

    Falta lo de hermanos de objetos

--- CaptchaForge/src/test/java/com/cunoc/CaptchaForge/AnalyzerCCTest.java
+++ CaptchaForge/src/test/java/com/cunoc/CaptchaForge/AnalyzerCCTest.java
@@ -23,7 +23,97 @@ public class AnalyzerCCTest {
 	}
 
 	@Test
-	void CCLanguageStructure() {
+	void CCHead() {
+		String CCLanguageStructure = "<C_CC>\n" + //
+						"  <C_HEAD>\n" + //
+						"    <C_TITLE></C_TITLE>\n" + //
+						"    <C_LINK/>\n" + //
+						"  </C_HEAD>\n" + //
+						"</C_CC>\n" + //
+						"";
+		AnalyzerCC analyzer = new AnalyzerCC(CCLanguageStructure);
+        analyzer.analyzer();
+		Assertions.assertTrue(!analyzer.isError());
+	}
+
+	@Test
+	void CCBodyChildless() {
+		String CCLanguageStructure = "<C_CC>\n" + //
+						"  <C_HEAD>\n" + //
+						"    <C_TITLE></C_TITLE>\n" + //
+						"    <C_LINK/>\n" + //
+						"  </C_HEAD>\n" + //
+						"  <C_BODY>\n" + //
+						"    \n" + //
+						"  </C_BODY>\n" + //
+						"</C_CC>";
+		AnalyzerCC analyzer = new AnalyzerCC(CCLanguageStructure);
+        analyzer.analyzer();
+		Assertions.assertTrue(!analyzer.isError());
+	}
+
+	@Test
+	void CCBodyWithDiv() {
+		String CCLanguageStructure = "<C_CC>\n" + //
+						"  <C_HEAD>\n" + //
+						"    <C_TITLE></C_TITLE>\n" + //
+						"    <C_LINK/>\n" + //
+						"  </C_HEAD>\n" + //
+						"  <C_BODY>\n" + //
+						"    <C_DIV>\n" + //
+						"    </C_DIV>\n" + //
+						"  </C_BODY>\n" + //
+						"</C_CC>";
+		AnalyzerCC analyzer = new AnalyzerCC(CCLanguageStructure);
+        analyzer.analyzer();
+		Assertions.assertTrue(!analyzer.isError());
+	}
+
+	@Test
+	void CCBodyWithTwoDiv() {
+		String CCLanguageStructure = "<C_CC>\n" + //
+						"  <C_HEAD>\n" + //
+						"    <C_TITLE></C_TITLE>\n" + //
+						"    <C_LINK/>\n" + //
+						"  </C_HEAD>\n" + //
+						"  <C_BODY>\n" + //
+						"    <C_DIV>\n" + //
+						"      \n" + //
+						"    </C_DIV>\n" + //
+						"    <C_DIV>\n" + //
+						"      \n" + //
+						"    </C_DIV>\n" + //
+						"    <C_DIV>\n" + //
+						"      \n" + //
+						"    </C_DIV>\n" + //
+						"  </C_BODY>\n" + //
+						"</C_CC>\n" + //
+						"";
+		AnalyzerCC analyzer = new AnalyzerCC(CCLanguageStructure);
+        analyzer.analyzer();
+		Assertions.assertTrue(!analyzer.isError());
+	}
+
+	@Test
+	void CCBodywithAnObjectInsideTheDiv() {
+		String CCLanguageStructure = "<C_CC>\n" + //
+						"  <C_HEAD>\n" + //
+						"    <C_TITLE></C_TITLE>\n" + //
+						"    <C_LINK/>\n" + //
+						"  </C_HEAD>\n" + //
+						"  <C_BODY>\n" + //
+						"    <C_DIV>\n" + //
+						"      <C_H1></C_H1>\n" + //
+						"    </C_DIV>\n" + //
+						"  </C_BODY>\n" + //
+						"</C_CC>\n" + //
+						"";
+		AnalyzerCC analyzer = new AnalyzerCC(CCLanguageStructure);
+        analyzer.analyzer();
+		Assertions.assertTrue(!analyzer.isError());
+	}
+	@Test
+	void CCBodyInternalThingsInTheDiv() {
 		String CCLanguageStructure = "<C_CC>\n" + //
 						"  <C_HEAD>\n" + //
 						"    <C_TITLE></C_TITLE>\n" + //
@@ -33,54 +123,13 @@ public class AnalyzerCCTest {
 						"    <C_DIV>\n" + //
 						"      <C_H1></C_H1>\n" + //
 						"      <C_P></C_P>\n" + //
-						"      <C_SPAM></C_SPAM>\n" + //
-						"      <C_BR/>\n" + //
-						"      <C_IMG/>\n" + //
-						"      <C_DIV>\n" + //
-						"        <C_INPUT/>\n" + //
-						"        <C_BR/>\n" + //
-						"        <C_TEXTAREA></C_TEXTAREA>\n" + //
-						"        <C_BR/>\n" + //
-						"        <C_SELECT>\n" + //
-						"          <C_OPTION></C_OPTION>\n" + //
-						"          <C_OPTION></C_OPTION>\n" + //
-						"          <C_OPTION></C_OPTION>\n" + //
-						"        </C_SELECT>\n" + //
-						"      </C_DIV>\n" + //
-						"      <C_DIV>\n" + //
-						"        <C_BUTTON></C_BUTTON>\n" + //
-						"        <C_BR/>\n" + //
-						"        <C_INPUT/>\n" + //
-						"        <C_BR/>\n" + //
-						"        <C_IMG/>\n" + //
-						"      </C_DIV>\n" + //
-						"    </C_DIV>\n" + //
-						"    <C_DIV>\n" + //
-						"      <C_H1></C_H1>\n" + //
-						"      <C_P></C_P>\n" + //
-						"      <C_SPAM></C_SPAM>\n" + //
-						"      <C_BR/>\n" + //
-						"      <C_DIV>\n" + //
-						"        <C_SELECT>\n" + //
-						"          <C_OPTION></C_OPTION>\n" + //
-						"          <C_OPTION></C_OPTION>\n" + //
-						"        </C_SELECT>\n" + //
-						"        <C_BR/>\n" + //
-						"        <C_TEXTAREA></C_TEXTAREA>\n" + //
-						"      </C_DIV>\n" + //
-						"      <C_DIV>\n" + //
-						"        <C_INPUT/>\n" + //
-						"        <C_BR/>\n" + //
-						"        <C_BUTTON></C_BUTTON>\n" + //
-						"        <C_BR/>\n" + //
-						"        <C_IMG/>\n" + //
-						"      </C_DIV>\n" + //
+
 						"    </C_DIV>\n" + //
 						"  </C_BODY>\n" + //
-						"</C_CC>";
+						"</C_CC>\n" + //
+						"";
 		AnalyzerCC analyzer = new AnalyzerCC(CCLanguageStructure);
         analyzer.analyzer();
 		Assertions.assertTrue(!analyzer.isError());
 	}
-
 }
