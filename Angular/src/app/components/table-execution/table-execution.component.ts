42f2c633ef6b4aae15ef933908d93c7528294e1c
Benjamín de Jesús <benjaminperez201831504@cunoc.edu.gt>
2024-10-23 14:32:12 -0600

    Termine con la vista de angular para la tabla de debuger

--- Angular/src/app/components/table-execution/table-execution.component.ts
+++ Angular/src/app/components/table-execution/table-execution.component.ts
@@ -8,83 +8,87 @@ import { DataValueDebbuge } from '../../model/SymbolTable/DataValueDebbuge';
   standalone: true,
   imports: [],
   templateUrl: './table-execution.component.html',
-  styleUrl: './table-execution.component.css'
+  styleUrl: './table-execution.component.css',
 })
 export class TableExecutionComponent {
-  @ViewChild('codeArea', { static: false }) codeArea!: ElementRef<HTMLDivElement>;
+  @ViewChild('codeArea', { static: false })
+  codeArea!: ElementRef<HTMLDivElement>;
   id!: string;
   private listArray: DataValueDebbuge[] = [];
-  textArea = ''
-  index = 0
-  
+  textArea = '';
+  index = 0;
+
   viewListArray: DataValueDebbuge[] = [];
 
-  constructor(private route: ActivatedRoute,private apiService: ApiAnalizerService
-    ,private renderer: Renderer2
+  constructor(
+    private route: ActivatedRoute,
+    private apiService: ApiAnalizerService,
+    private renderer: Renderer2
   ) {}
 
-  nextList(){
-    this.colorDiv()
-    this.index++
+  nextList() {
+    this.index++;
+    this.colorDiv();
   }
 
   ngOnInit(): void {
-
-
     // Usando snapshot (captura instantánea de los parámetros)
     this.id = this.route.snapshot.paramMap.get('id')!;
 
-    // Listado Symbol 
-    this.apiService.getSymbolTable(this.id).subscribe(
-      data => data.forEach((element) => {
+    // Listado Symbol
+    this.apiService.getSymbolTable(this.id).subscribe((data) =>
+      data.forEach((element) => {
         const dataValueDebbuge = DataValueDebbuge.fromJSON(element);
         this.listArray.push(dataValueDebbuge);
       })
-    ),(error) => 
-      console.error('Error al obtener el listado de captchas:', error);
-    ;
-
-    console.log(this.listArray)
+    ),
+      (error) =>
+        console.error('Error al obtener el listado de captchas:', error);
+    console.log(this.listArray);
 
     // Texto de area
-    this.apiService.getStringScripting(this.id).subscribe(
-      data => {
-        if (!data) {
-          console.warn('La respuesta es un string vacío.');
-        } else {
-          this.textArea = data.toString()
-        }
+    this.apiService.getStringScripting(this.id).subscribe((data) => {
+      if (!data) {
+        console.warn('La respuesta es un string vacío.');
+      } else {
+        this.textArea = data.toString();
       }
-    ),(error) => 
-      console.error('Error al obtener el listado de captchas:', error);
-    ;
-    console.log(this.textArea)
+    }),
+      (error) =>
+        console.error('Error al obtener el listado de captchas:', error);
   }
 
   ngAfterViewInit() {
-    if (this.codeArea) { // Verifica que codeArea no sea undefined
+    if (this.codeArea) {
+      // Verifica que codeArea no sea undefined
       this.colorDiv();
     } else {
       console.error('codeArea no está disponible');
     }
   }
-  colorDiv(){
-// Dividir el texto en líneas
-const lines = this.textArea.split('\n');
+  colorDiv() {
+    // Limpiar el contenido previo
+    this.renderer.setProperty(this.codeArea.nativeElement, 'innerHTML', '');
 
-lines.forEach((line, idx) => {
-  // Crear un nuevo div para cada línea
-  const nuevoDiv = this.renderer.createElement('div');
-  const text = this.renderer.createText(line);
-  this.renderer.appendChild(nuevoDiv, text);
+    // Dividir el texto en líneas
+    const lines = this.textArea.split('\n');
 
-  // Colorear la línea si es la indicada por this.index
-  if (idx === this.index) {
-    this.renderer.setStyle(nuevoDiv, 'color', 'red'); // Colocar texto en rojo solo en la línea indicada
-  }
+    this.index = this.index >= lines.length ? 0 : this.index;
 
-  // Insertar el nuevo div dentro del div existente
-  this.renderer.appendChild(this.codeArea.nativeElement, nuevoDiv);
-});
+    lines.forEach((line, idx) => {
+      line = !(/^\s*$/.test(line) && idx === this.index) ? line : '####';
+      // Crear un nuevo div para cada línea
+      const nuevoDiv = this.renderer.createElement('div');
+      const text = this.renderer.createText(line);
+      this.renderer.appendChild(nuevoDiv, text);
+
+      // Colorear la línea si es la indicada por this.index
+      if (idx === this.index) {
+        this.renderer.setStyle(nuevoDiv, 'color', 'red'); // Colocar texto en rojo solo en la línea indicada
+      }
+
+      // Insertar el nuevo div dentro del div existente
+      this.renderer.appendChild(this.codeArea.nativeElement, nuevoDiv);
+    });
   }
 }
